510f73477cc7f2bc8cbfee0f398934e85e8f108d
Ismadbek <ismadbek@Ismadbeks-MacBook-Air.local>
2025-08-20 10:55:47 +0500

    uu

---
+++ prisma/migration.sql
@@ -0,0 +1,13 @@
+-- Add credit repayment tracking fields to PaymentSchedule table
+ALTER TABLE "PaymentSchedule" 
+ADD COLUMN "creditRepaymentAmount" DOUBLE PRECISION DEFAULT 0,
+ADD COLUMN "repaymentDate" TIMESTAMP(3);
+
+-- Add credit repayment tracking fields to Transaction table
+ALTER TABLE "Transaction" 
+ADD COLUMN "creditRepaymentAmount" DOUBLE PRECISION DEFAULT 0,
+ADD COLUMN "lastRepaymentDate" TIMESTAMP(3);
+
+-- Update existing records to have default values
+UPDATE "PaymentSchedule" SET "creditRepaymentAmount" = 0 WHERE "creditRepaymentAmount" IS NULL;
+UPDATE "Transaction" SET "creditRepaymentAmount" = 0 WHERE "creditRepaymentAmount" IS NULL;
